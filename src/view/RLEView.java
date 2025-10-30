44ca5400a12bfd157513ecd00fbef49ab646be37
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-10-30 07:54:56 +0100

    add lib

--- src/view/RLEView.java
+++ src/view/RLEView.java
@@ -3,19 +3,23 @@ package view;
 import javax.swing.JButton;
 
 import core.AbstractView;
+import schimkat.berlin.lernhilfe2025ws.welcome.LittleDialog;
 
 public class RLEView extends AbstractView {
 
 	private JButton switchButton = new JButton("Zum Rechner");
+	
 
 	public RLEView(MainWindow frame) {
 		super(frame);
 		this.switchButton.addActionListener(e -> flip(Cards.CALCULATOR));
 		this.add(switchButton);
+		
 	}
 
 	@Override
 	public void onFlip() {
+		LittleDialog.sayHello();
 		this.setTitle("RLE");
 	}
 
